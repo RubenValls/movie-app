d113838aa33b6c3212552077d7951f74e669ab9c
Rubén Valls <r.vallsaparici@gmail.com>
2023-09-02 13:14:26 +0200

    added movies visualization sorted by popularity

--- scripts.js
+++ scripts.js
@@ -9,8 +9,22 @@ const getMovies = () => {
         .then(response => response.json())
         .then(data => { 
             movies = [...data.results];
+            printMovies(movies);
         })
         .catch(error => console.log(error));
+}
+
+const printMovies = (movies = []) => {
+    const moviesContainer = document.querySelector('.movies-container');
+    movies.forEach((movie) => {
+        moviesContainer.innerHTML += `<div class="movie-card" id="${movie.original_title}">
+            <img src="${IMG_PATH + movie.poster_path}" alt="test">
+            <div class="movie-info">
+                <h3>${movie.original_title}</h3>
+                <div class="movie-rating">${movie.vote_average.toFixed(1)}</div>
+            </div>
+            </div>`
+    })
 }
 
 getMovies()
